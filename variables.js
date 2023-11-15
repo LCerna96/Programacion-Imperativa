1053f59a82bacb210156f496f7f4678e9d8eea65
LCerna96 <luisenrique0104@gmail.com>
2023-11-15 03:09:33 -0500

    primer commit

---
+++ variables.js
@@ -0,0 +1,56 @@
+// ANCHOR ----> Variables
+
+// let - const
+
+let edad = 25;
+
+let nombre = "Luis";
+
+const apellido = "Cerna";
+console.log(edad);
+console.log(nombre);
+console.log(apellido);
+
+// ANCHOR ----> Tipos de datos
+
+// STRINGS --> cadenas de texto
+
+let frase = "hola como estas?";
+let frase2 = "hola como estás?";
+
+// CONCATENACIÓN
+
+let nombreUsuario = "Pepito";
+
+console.log("hola " + nombreUsuario);
+
+// TEMPLATE LITERAL ----> backticks (interpolación)
+
+console.log(`Hola ${nombreUsuario} como estás?`);
+
+// NUMBERS --> números
+
+let altura = 176.6;
+let telefono = "1234567890";
+
+console.log(typeof altura);
+console.log(typeof telefono);
+
+// BOOLEANS --> booleanos --> true or false
+
+let estaLogeado = true;
+
+console.log(typeof estaLogeado);
+
+// ANCHOR ----> Operadores
+
+// MATEMATICOS
+
+let a = 2;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
