ecf87e1fc8fa487d0b11090560dc72d837b07154
LCerna96 <luisenrique0104@gmail.com>
2023-11-16 00:29:48 -0500

    commit miercoles 2

--- funcion.js
+++ funcion.js
@@ -8,8 +8,48 @@ console.log(nombre);
 
 // Declaradas
 
-function f1(params) {}
+function f1() {
+  let edad = 5;
+  console.log(edad);
+}
 
-function f1(params) {}
+f1();
 
-function f1(params) {}
+//
+
+function saludar(nombre) {
+  let saludo = "Hola cómo estás" + nombre;
+  return saludo;
+}
+
+let saludoFinal = saludar("martin");
+console.log(saludoFinal);
+
+let nombreUsuario = "pepito";
+
+function cambiarNombre() {
+  nombreUsuario = "maria";
+}
+
+console.log(nombreUsuario);
+
+cambiarNombre();
+
+console.log(nombreUsuario);
+
+console.log("-----");
+
+//
+
+function sumar(numeroUno, numeroDos) {
+  return numeroUno + numeroDos;
+}
+
+let res = sumar(10, 20);
+console.log(res);
+
+let res2 = sumar(5, 7);
+console.log(res2);
+
+let res3 = sumar(6, 1);
+console.log(res3);
